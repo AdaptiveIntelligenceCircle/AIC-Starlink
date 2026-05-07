a559071018b7c63ab2d66e2064c164c0f73839fb
ndtribk <ndtribk@gmail.com>
2026-05-07 10:04:16 +0700

    update

---
+++ include/aic-starlink/telemetry/telemetry_metrics.h
@@ -0,0 +1,38 @@
+#pragma once 
+
+#include <cstdint>
+
+#include <deque> 
+
+#include "aic-starlink/telemetry/telemetry_event.h"
+
+using namespace std;
+
+namespace aic_starlink :: telemetry 
+{
+    struct MetricSummary 
+    {
+        double avg_latency_ms = 0.0; 
+        double avg_jitter_ms = 0.0; 
+        double avg_loss = 0.0; 
+
+        double max_latency_ms = 0.0; 
+        double max_loss = 0.0; 
+
+        size_t samples = 0; 
+    }; 
+
+    class Metrics 
+    {
+        public: 
+        explicit Metrics(size_t window_size = 0); 
+
+        void ingest(const TelemetryEvent &ev);
+
+        [[nodiscard]] MetricSummary summarize() const noexcept; 
+
+        private: 
+        size_t m_window_size; 
+        deque<LinkMetrics> m_recent; 
+    }; 
+}
