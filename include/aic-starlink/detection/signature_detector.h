7706f15efdf6a5084661815104ad9be809a79645
ndtribk <ndtribk@gmail.com>
2026-05-08 18:25:31 +0700

    update

---
+++ include/aic-starlink/detection/signature_detector.h
@@ -0,0 +1,37 @@
+#pragma once 
+
+#include <string>
+
+#include "aic-starlink/telemetry/telemetry_event.h"
+using namespace std; 
+
+namespace aic_starlink :: detection 
+{
+    enum class SignatureType 
+    {
+        None,
+        JamingPattern, 
+        SpoofingPattern, 
+        TelemetryInjectionPattern, 
+        ReplayPattern 
+    }; 
+
+    struct SignatureResult 
+    {
+        SignatureType type 
+        {
+            SignatureType :: None
+        };
+
+        double confidence = 0.0; 
+        string reason; 
+    }; 
+
+    class SignatureDetector 
+    {
+        public:
+        SignatureDetector() = default; 
+
+        [[nodiscard]] SignatureResult evaluate(const telemetry :: TelemetryEvent &ev) const; 
+    };  
+}
